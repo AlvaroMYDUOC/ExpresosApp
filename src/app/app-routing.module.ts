0ac5159e947e061d723a2cd23a63981a8d09bf26
AlvaroMYDUOC <alv.madrid@duocuc.cl>
2022-06-22 01:32:50 -0400

    First Comit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,16 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('src/app/home/home.module').then( m => m.HomePageModule)
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('src/app/usuario/usuario.module').then( m => m.UsuarioPageModule)
   },
 ];
 
